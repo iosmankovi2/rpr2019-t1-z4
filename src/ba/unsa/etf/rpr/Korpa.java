e24904f343a0d2a9eb7525fcbe4dab56285cf3c4
iosmankovi2 <iosmankovi2@etf.unsa.ba>
2020-11-03 15:08:01 +0100

    Second

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,53 @@
 package ba.unsa.etf.rpr;
 
+import java.util.*;
+
 public class Korpa {
+
+    private Artikl[] artikl = new Artikl[50]; //maksimalno 50 artikala
+    private int broj = 0;
+    public boolean dodajArtikl(Artikl a) {
+
+        if(broj < 50) {
+            artikl[broj] = new Artikl(a.getNaziv(), a.getCijena(), a.getKod());
+            broj++;
+            return  true;
+    }
+        return  false;
+    }
+
+    public Artikl[] getArtikl() {
+        return artikl;
+    }
+
+    public <String> Artikl izbaciArtiklSaKodom(String kod) {
+        for(int i = 0; i < broj; i++) {
+
+            if(artikl[i].getKod().equals(kod)) {
+                Artikl novi = new Artikl(artikl[i].getNaziv(), artikl[i].getCijena(), artikl[i].getKod());
+                artikl[i] = null;
+                pomjeriSveZa(i);
+                return novi;
+            }
+        }
+        return null;
+    }
+
+    public int dajUkupnuCijenuArtikla() {
+        int ukupna_cijena = 0;
+        for(int i = 0; i < broj; i++) {
+            ukupna_cijena = ukupna_cijena + artikl[i].getCijena();
+        }
+            return ukupna_cijena;
+    }
+
+    private void pomjeriSveZa(int pozicija) {
+        for(int i = pozicija; i < broj; i++) {
+            artikl[i] = artikl[i+1];
+        }
+        artikl[broj] = null;
+        broj--;
+    }
+
+
 }
